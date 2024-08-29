babad8777d6fc7de3a45f957975f0eb62c352b03
Anseongmin5739 <smahn4069@gmail.com>
2024-08-30 02:42:58 +0900

    config수정

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
-    outDir: 'dist', // 빌드 결과물이 나오는 디렉토리
+    outDir: 'dist',
   },
 });
+
