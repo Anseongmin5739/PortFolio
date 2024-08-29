c3ad87d23affba5bcab5f6ed2692e5a707be5c00
Anseongmin5739 <smahn4069@gmail.com>
2024-08-30 02:32:51 +0900

    헤더 수정

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -10,7 +10,6 @@ const Header = () => {
           <li><Link to="about" smooth={true} duration={500}>About</Link></li>
           <li><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
           <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
-          <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
         </ul>
       </nav>
     </header>
