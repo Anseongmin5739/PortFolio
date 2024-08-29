886e26a3e9a2124a66ae0f75384eb1fdcf966c36
Anseongmin5739 <smahn4069@gmail.com>
2024-08-29 11:53:09 +0900

    first ProtFolio

---
+++ portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>010-9722-5739</p>
+    </section>
+  );
+}
+
+export default Contact;
